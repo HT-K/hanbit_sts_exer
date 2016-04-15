7859d58daaa12616cf5471fbd6b1fbe4bf28e2ba
김희태 <propose0506@naver.com>
2016-04-15 14:23:42 +0900

    Junit 테스트 완료

--- hanbit/src/test/java/com/hanbit/web/member/MemberServiceImplTest.java
+++ hanbit/src/test/java/com/hanbit/web/member/MemberServiceImplTest.java
@@ -2,24 +2,29 @@ package com.hanbit.web.member;
 
 import static org.junit.Assert.*;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.hanbit.web.mapper.MemberMapper;
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:META-INF/*-context.xml")
+@ContextConfiguration("classpath:META-INF/*-context.xml") 
 public class MemberServiceImplTest {
-	@Autowired 
-	
-	@Before
-	public void setUp() throws Exception {
-	}
+	// 이 AutoWired를 쓰려면 servlet-context.xml을 인식하게 해야한다. 그래서 위에 contetConfiguration을 쓰고 servlet-context.xml을 META-INF밑으로 옮겨온다.
+	@Autowired MemberDTO member;
+	@Autowired SqlSession session;
 
 	@Test
 	public void testLogin() {
-		fail("Not yet implemented");
+		MemberMapper mapper = session.getMapper(MemberMapper.class);
+		member.setId("hong");
+		member.setPassword("1");
+		member = mapper.login(member);
+		assertEquals("홍길동", member.getName());
 	}
 
 }
